63b9de8039e4d6e750f082ca617873d5f2079ebc
Peter Armstrong <peteretep@gmail.com>
2012-04-23 22:47:16 +0100

    sfd

--- FileSearcher.java
+++ FileSearcher.java
@@ -17,31 +17,38 @@ public class FileSearcher
 	 * @throws FileNotFoundException 
 	 */
 	ArrayList <String> words;
-	ArrayList <String> files;
-	
+	ArrayList <String> fileContent;
+	ArrayList <String> fileNames;
 	public static void main(String[] args) throws IOException 
 	{
 		FileSearcher filesearch1=new FileSearcher();
 		filesearch1.addFiles("file1.txt");
 		filesearch1.addFiles("file2.txt");
 		filesearch1.addWords();
-		for (int j=0;j<filesearch1.files.size();j++)
+		for (int j=0;j<filesearch1.fileContent.size();j++)
 		{
 			int times = filesearch1.checkIfWordIsInFiles(j);
-			System.out.println(times);
+			if (times!=0)
+			{
+				System.out.println(filesearch1.fileNames.get(j));
+				System.out.println(times);
+			}
+			
 		}
 
 	}
 
 	private FileSearcher() throws FileNotFoundException
 	{
-		files= new ArrayList<String>();		
+		fileContent= new ArrayList<String>();		
+		fileNames = new ArrayList<String>();
 		words= new  ArrayList<String>();
 	}
 	
-	private void addFiles(String filelocationstring) throws IOException
+	private void addFiles(String fileLocationString) throws IOException
 	{
-		FileInputStream file= new FileInputStream(filelocationstring);
+		fileNames.add(fileLocationString);
+		FileInputStream file= new FileInputStream(fileLocationString);
 	
 		BufferedReader in=new BufferedReader(new InputStreamReader(file));
 		String str;
@@ -51,7 +58,7 @@ public class FileSearcher
 			fileString=fileString+ " "+ str;				
 		}
 		
-		files.add(fileString);
+		fileContent.add(fileString);
 	}
 	
 	private void addWords() 
@@ -67,7 +74,7 @@ public class FileSearcher
 		for(int i=0; i<words.size();i++)
 		{
 			
-			if (files.get(j).contains(words.get(i)))
+			if (fileContent.get(j).contains(words.get(i)))
 			{
 				times++;
 			}
